7e61fc8a86a4bca48070523f4cca0024c0ef80ff
RestedWickedDev <jesusdperez@gmail.com>
2020-12-06 20:28:11 -0800

    Flask or VSCode failed me

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 @app.route('/')
 def index():
@@ -12,4 +21,3 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=True)
-
